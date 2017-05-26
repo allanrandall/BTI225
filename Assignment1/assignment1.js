adaf1a6668ca3fd42713f621666ee8fa47247fe9
allanrandall <allan.randall@senecacollege.ca>
2017-05-26 11:09:36 -0400

    Add files via upload

---
+++ Assignment1/assignment1.js
@@ -0,0 +1,75 @@
+
+
+
+
+
+
+
+/********************************
+ *          TEST DATA           *
+ *  write all of your functions *
+ *      ABOVE this comment      *
+ *                              *
+ *  NOTE: the code below simply *
+ *  invokes each function to    *
+ *  show the result - it does   *
+ *  not state whether the       *
+ *  output is correct or not    *
+ ********************************/
+
+// Function 1 (grader)   
+
+console.log('grader(53) returns: ' + grader(53));
+console.log('grader(72) returns: ' + grader(72));
+console.log('grader(91) returns: ' + grader(91));
+
+console.log("\n");
+
+// Function 2 (showMultiples)
+
+console.log('showMultiples(2,8) returns: ' + showMultiples(2,8));
+console.log('showMultiples(3,2) returns: ' + showMultiples(3,2));
+console.log('showMultiples(5,4) returns: ' + showMultiples(5,4));
+
+console.log("\n");
+
+// Function 3 (largerNum)
+  
+console.log('largerNum(2095,106) returns: ' + largerNum(2095,106));
+console.log('largerNum("23",14) returns: ' + largerNum("23",14));
+console.log('largerNum(108,"a") returns: ' + largerNum(108,"a"));
+
+console.log("\n");
+
+// Function 4 (tempConvert)
+
+console.log('tempConvert(22,"CF") returns: ' + tempConvert(22,"CF"));
+console.log('tempConvert(76,"FC") returns: ' + tempConvert(76,"FC"));
+console.log('tempConvert(16,"CF") returns: ' + tempConvert(16,"CF"));
+
+console.log("\n");
+
+// Function 5 (evenNumbers)
+
+console.log('evenNumbers(4,13) returns: ' + evenNumbers(4,13));
+console.log('evenNumbers(3,10) returns: ' + evenNumbers(3,10));
+console.log('evenNumbers(8,21) returns: ' + evenNumbers(8,21));
+
+console.log("\n");
+
+// Function 6 (passingAverage)
+
+console.log('passingAverage(75,42,98) returns: ' + passingAverage(75,42,98));
+console.log('passingAverage(34,93,77,89,49) returns: ' + passingAverage(34,93,77,89,49));
+console.log('passingAverage(33,61) returns: ' + passingAverage(33,61));
+
+console.log("\n");
+
+// Functinon 7 (counter)
+
+var count = counter();
+
+console.log('counter() returns: ' + count);
+console.log('invoking the function returns: ' + count());
+console.log('invoking the function returns: ' + count());
+console.log('invoking the function returns: ' + count());
